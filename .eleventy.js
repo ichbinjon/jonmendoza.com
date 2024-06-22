1ad0d2ba2edf962d3a4213f306a9c8a1e0a05dd9
ichbinjon <jpgmend@gmail.com>
2024-06-22 17:36:28 +0100

    add assets to the website

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,14 @@
 module.exports = function(eleventyConfig){
+  eleventyConfig.addPassthroughCopy("./src/bundle.css");
+  eleventyConfig.addPassthroughCopy({ "favicon": "/" });
     return {
       dir: {
         input: "src",
-        data: "_data",
-        includes: "_includes",
-        layouts: "_layouts",
+        // data: "_data",
+        // includes: "_includes",
+        // layouts: "_layouts",
         output: "docs"
-      }
+      },
+      passthroughFileCopy: true
     };
   }
